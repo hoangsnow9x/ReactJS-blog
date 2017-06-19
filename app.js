000b5d64454886ffc5154bdbf680f1475d7ba807
hoangsnow9x <hoangsnow172@gmail.com>
2017-06-19 17:41:33 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// npm init
+// npm install  babel-preset-es2015
+// npm install nodemon
+// npm install morgan body-parser
+// npm install babel-preset-stage-0
+// npm install react
+// npm install react-dom
+require('babel-register');
+require('babel-polyfill');
+require('./server');
